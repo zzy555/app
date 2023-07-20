6d372b3f74c00b90f671b9fceba824fa1dff387a
zzy <1114767919@qq.com>
2023-07-20 18:48:33 +0800

    add

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ func main() {
 	// 2.绑定路由规则，执行的函数
 	// gin.Context，封装了request和response
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello World!")
+		c.String(http.StatusOK, "welcome")
 	})
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
-	r.Run(":8000")
+	r.Run(":8001")
 }
